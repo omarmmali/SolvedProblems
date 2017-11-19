fbb338a12d73b6762666c063b6644f6bbe0ff5f4
Omar M. Ali <omarmaboelenen@gmail.com>
2017-11-19 09:32:02 +0200

    updating

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    long double n;
-    while(cin >> n) {
-        if(n == 0) {
-            break;
-        }
-        long double a;
-        for(int i = 1; ; i++) {
-            if(i * i * i > n) {
-                a = i - 1;
-                break;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    string in;
+    cin >> in;
+    set<pair<int,int> > intervals;
+    int res = 0;
+    for(int i = 0; i < n; i++) {
+        if(in[i] == '.') {
+            pair<int,int> cur_interval = make_pair(i, -1);
+            while(in[i] == '.' && i < n) {
+                i++;
             }
+            cur_interval.second = i - 1;
+            intervals.insert(cur_interval);
+            res += cur_interval.second - cur_interval.first;
         }
-        long double dx = (n - (a * a * a)) / (3 * a * a); 
-        cout << fixed << setprecision(4) << a + dx << endl;
+    }
+    while(q--) {
+
     }
     return 0;
 }
