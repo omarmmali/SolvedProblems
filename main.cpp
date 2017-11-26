8105f24b5dc088ac14a878fad4aa4654a166d51c
Omar M. Ali <omarmaboelenen@gmail.com>
2017-11-26 13:01:23 +0200

    Updating added codeblocks project

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+*/
+
+typedef long long ll;
+int coins[10];
+int dp[3][1000000];
+int n;
+ll solve(int cur, int tot) {
+    if(tot > n) {
+        return 0;
+    }
+    if(cur == 3) {
+        return (tot == n);
+    }
+    if(dp[cur][tot] != -1) {
+        return dp[cur][tot];
+    }
+    ll ret = 0;
+    for(int i = 0; i <= n / coins[cur]; i++) {
+        ret += solve(cur + 1, tot + (i * coins[cur]));
+    }
+    return dp[cur][tot] = ret;
+}
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, q;
-    cin >> n >> q;
-    string in;
-    cin >> in;
-    set<pair<int,int> > intervals;
-    int res = 0;
-    for(int i = 0; i < n; i++) {
-        if(in[i] == '.') {
-            pair<int,int> cur_interval = make_pair(i, -1);
-            while(in[i] == '.' && i < n) {
-                i++;
-            }
-            cur_interval.second = i - 1;
-            intervals.insert(cur_interval);
-            res += cur_interval.second - cur_interval.first;
+    int t;
+    while(cin >> t) {
+        n = t - 1;
+        coins[0] = 1;
+        coins[1] = 2;
+        coins[2] = ceil(max(1,n) / 3.0);
+        memset(dp, -1, sizeof(dp));
+        int tmp = solve(0, 0);
+        if(tmp != t) {
+            cout << n << ' ' << coins[2] << ' ' << tmp << endl;
         }
     }
-    while(q--) {
-
-    }
     return 0;
-}
+}   
